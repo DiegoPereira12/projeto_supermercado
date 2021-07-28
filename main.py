50470bb0d63429f405ac449e11b92fa5d918cc07
DiegoPereira12 <diegoo.junior55@gmail.com>
2021-07-27 23:26:36 -0300

    Tratando erros e exeções

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from os import system
+from gera_csv import gerarCsv
 import modulo_sup
 from pacote.sup_alimentos import lista_alimentos
 from pacote.sup_bebidas import lista_bebidas
 from pacote.sup_acougue import lista_carnes
 
-# Menu principal 
 
 while True:
 
@@ -15,47 +15,34 @@ while True:
     print('[3] - Bebidas')
     modulo_sup.espaco()
 
-    opcao = int(input('Digite sua opção: '))
-    if opcao == 1:
-        system('cls')
-        modulo_sup.cabecalho('MENU ALIMENTOS')
-        print(lista_alimentos)
+    try:
+        opcao = 1 or 2 or 3
+        opcao = int(input('Digite sua opção: '))
         modulo_sup.espaco()
-        modulo_sup.menu_alimentos()
-
-    elif opcao == 2:
         system('cls')
-        modulo_sup.cabecalho('MENU AÇOUGUE')
-        print(lista_carnes)
-        modulo_sup.espaco()
-        modulo_sup.menu_acougue()
+        print("\033[0;31m--- ERRO! DIGITE UMA OPÇÃO VÁLIDA ---\033[m")
 
-    elif opcao == 3:
+        if opcao == 1:
+            system('cls')
+            modulo_sup.cabecalho('MENU ALIMENTOS')
+            print(lista_alimentos)
+            modulo_sup.espaco()
+            modulo_sup.menu_alimentos()
+
+        elif opcao == 2:
+            system('cls')
+            modulo_sup.cabecalho('MENU AÇOUGUE')
+            print(lista_carnes)
+            modulo_sup.espaco()
+            modulo_sup.menu_acougue()
+
+        elif opcao == 3:
+            system('cls')
+            modulo_sup.cabecalho('MENU BEBIDAS')
+            print(lista_bebidas)
+            modulo_sup.espaco()
+            modulo_sup.menu_bebidas()
+    except:
         system('cls')
-        modulo_sup.cabecalho('MENU BEBIDAS')
-        print(lista_bebidas)
-        modulo_sup.espaco()
-        modulo_sup.menu_bebidas()
-        
-
-
-
-    
-    
-    
-        
-        
-
-        
-        
-        
-
-
-
-
-
-
-
-
-
+    print("\033[0;31m--- ERRO! DIGITE UMA OPÇÃO VÁLIDA ---\033[m")
     
