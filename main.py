d447cd1bcb3378bf53fc764d50b8476774fdb131
DiegoPereira12 <diegoo.junior55@gmail.com>
2021-08-03 12:36:52 -0300

    Novos teste de validação de entrada

--- main.py
+++ main.py
@@ -1,48 +1,32 @@
 from os import system
-from gera_csv import gerarCsv
+import sys
 import modulo_sup
+import gera_csv
 from pacote.sup_alimentos import lista_alimentos
 from pacote.sup_bebidas import lista_bebidas
 from pacote.sup_acougue import lista_carnes
 
-
 while True:
-
-    modulo_sup.cabecalho('MENU INICIAL')
-    print('Escolha uma das opções abaixo:')
-    print('[1] - Alimentos em geral')
-    print('[2] - Açougue')
-    print('[3] - Bebidas')
-    modulo_sup.espaco()
-
-    try:
-        opcao = 1 or 2 or 3
-        opcao = int(input('Digite sua opção: '))
-        modulo_sup.espaco()
-        system('cls')
-        print("\033[0;31m--- ERRO! DIGITE UMA OPÇÃO VÁLIDA ---\033[m")
-
-        if opcao == 1:
-            system('cls')
-            modulo_sup.cabecalho('MENU ALIMENTOS')
-            print(lista_alimentos)
-            modulo_sup.espaco()
-            modulo_sup.menu_alimentos()
-
-        elif opcao == 2:
-            system('cls')
-            modulo_sup.cabecalho('MENU AÇOUGUE')
-            print(lista_carnes)
-            modulo_sup.espaco()
-            modulo_sup.menu_acougue()
-
-        elif opcao == 3:
-            system('cls')
-            modulo_sup.cabecalho('MENU BEBIDAS')
-            print(lista_bebidas)
-            modulo_sup.espaco()
-            modulo_sup.menu_bebidas()
-    except:
-        system('cls')
-    print("\033[0;31m--- ERRO! DIGITE UMA OPÇÃO VÁLIDA ---\033[m")
     
+    modulo_sup.cabecalho()
+    
+    opcao = input('Digite sua opção: ')
+    system('cls')
+
+    if opcao == '1':
+        system('cls')
+        print(lista_alimentos)
+        modulo_sup.espaco()
+        modulo_sup.menu_alimentos()
+ 
+    elif opcao == '2':
+        system('cls')
+        print(lista_carnes)
+        modulo_sup.espaco()
+        modulo_sup.menu_acougue()
+ 
+    elif opcao == '3':
+        system('cls')
+        print(lista_bebidas)
+        modulo_sup.espaco()
+        modulo_sup.menu_bebidas()
